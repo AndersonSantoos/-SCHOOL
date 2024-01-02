faa25064ea8ba8a833a129ee7e5e64e892d42961
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2024-01-02 20:29:51 -0300

    Padronização

--- src/repository/profRepositorio.js
+++ src/repository/profRepositorio.js
@@ -1,8 +1,8 @@
 const AcompanhamentoProfessor = require('../models/professorModel');
-
+const db = require("../db/dbConfig");
 class AcompanhamentoRepository {
     constructor() {
-        this.db = require('../db/dbConfig');
+        
         this.acompanhamentoProfessor = new AcompanhamentoProfessor();
     }
 
@@ -12,7 +12,7 @@ class AcompanhamentoRepository {
                 throw new Error('Todos os campos devem ser preenchidos.');
             }
             const query = 'INSERT INTO eventos_acompanhamento (aluno, tipo_evento, descricao, relato, visao_geral) VALUES (?, ?, ?, ?, ?)';
-            await this.db.query(query, [aluno, 'Briga', descricao, relato, visaoGeral]);
+            await db.query(query, [aluno, 'Briga', descricao, relato, visaoGeral]);
 
             this.acompanhamentoProfessor.registrarEvento(aluno, 'Briga', descricao);
             this.acompanhamentoProfessor.registrarVisaoGeralTurma(visaoGeral);
@@ -31,7 +31,7 @@ class AcompanhamentoRepository {
             }
     
             const query = 'INSERT INTO eventos_acompanhamento (aluno, tipo_evento, descricao, relato, visao_geral) VALUES (?, ?, ?, ?, ?)';
-            await this.db.query(query, [aluno, 'Rendimento Abaixo', descricao, relato, visaoGeral]);
+            await db.query(query, [aluno, 'Rendimento Abaixo', descricao, relato, visaoGeral]);
     
             this.acompanhamentoProfessor.registrarEvento(aluno, 'Rendimento Abaixo', descricao);
             this.acompanhamentoProfessor.registrarVisaoGeralTurma(visaoGeral);
@@ -50,7 +50,7 @@ class AcompanhamentoRepository {
             }
     
             const query = 'INSERT INTO eventos_acompanhamento (aluno, tipo_evento, descricao, relato, visao_geral) VALUES (?, ?, ?, ?, ?)';
-            await this.db.query(query, [aluno, 'Pouca Participacao', descricao, relato, visaoGeral]);
+            await db.query(query, [aluno, 'Pouca Participacao', descricao, relato, visaoGeral]);
     
             this.acompanhamentoProfessor.registrarEvento(aluno, 'Pouca Participacao', descricao);
             this.acompanhamentoProfessor.registrarVisaoGeralTurma(visaoGeral);
@@ -69,7 +69,7 @@ class AcompanhamentoRepository {
             }
     
             const query = 'INSERT INTO eventos_acompanhamento (aluno, tipo_evento, descricao, relato, visao_geral) VALUES (?, ?, ?, ?, ?)';
-            await this.db.query(query, [aluno, 'Alto Rendimento', descricao, relato, visaoGeral]);
+            await db.query(query, [aluno, 'Alto Rendimento', descricao, relato, visaoGeral]);
     
             this.acompanhamentoProfessor.registrarEvento(aluno, 'Alto Rendimento', descricao);
             this.acompanhamentoProfessor.registrarVisaoGeralTurma(visaoGeral);
@@ -88,7 +88,7 @@ class AcompanhamentoRepository {
             }
     
             const query = 'INSERT INTO eventos_acompanhamento (aluno, tipo_evento, descricao, relato) VALUES (?, ?, ?, ?)';
-            await this.db.query(query, [aluno, 'Relato Extra', relato, null]);
+            await db.query(query, [aluno, 'Relato Extra', relato, null]);
     
             this.acompanhamentoProfessor.registrarEvento(aluno, 'Relato Extra', relato);
 
@@ -106,7 +106,7 @@ class AcompanhamentoRepository {
             }
     
             const query = 'INSERT INTO visao_geral_turma (descricao) VALUES (?)';
-            await this.db.query(query, [visaoGeral]);
+            await db.query(query, [visaoGeral]);
     
             this.acompanhamentoProfessor.registrarVisaoGeralTurma(visaoGeral);
 
@@ -128,7 +128,7 @@ class AcompanhamentoRepository {
             console.log("Valores da consulta:", descricao, relato, visaoGeral, aluno, tipoEvento, id);
     
             const query = 'UPDATE eventos_acompanhamento SET descricao = ?, relato = ?, visao_geral = ?, aluno = ?, tipo_evento = ? WHERE id = ?';
-            await this.db.execute(query, [descricao, relato, visaoGeral, aluno, tipoEvento, id]);
+            await db.execute(query, [descricao, relato, visaoGeral, aluno, tipoEvento, id]);
     
             console.log('Acompanhamento atualizado com sucesso.');
         } catch (error) {
@@ -141,7 +141,7 @@ class AcompanhamentoRepository {
     async recuperarAcompanhamento(id) {
         try {
             const query = 'SELECT * FROM eventos_acompanhamento WHERE id = ? AND status != ?';
-            const [acompanhamento] = await this.db.query(query, [id, 'excluido']);
+            const [acompanhamento] = await db.query(query, [id, 'excluido']);
     
             return acompanhamento[0];
         } catch (error) {
@@ -160,7 +160,7 @@ class AcompanhamentoRepository {
             }
             
             const query = 'UPDATE eventos_acompanhamento SET status = ?, versao = versao + 1 WHERE id = ?';
-            await this.db.query(query, ['excluido', id]);
+            await db.query(query, ['excluido', id]);
     
             console.log('Acompanhamento excluído com sucesso.');
             return true;
--- src/routes/profRoutes.js
+++ src/routes/profRoutes.js
@@ -4,22 +4,22 @@ const router = express.Router();
 const acompanhamentoController = new AcompanhamentoController();
 
 // Rota para cadastrar um evento
-router.post('/cadastrar-evento', async (req, res) => {
+router.post('/add_prof', async (req, res) => {
     await acompanhamentoController.cadastrarEvento(req, res);               //FUNCIONANDO
 });
 
 // Rota para atualizar um acompanhamento
-router.put('/atualizar-acompanhamento/:id', async (req, res) => {
+router.put('/edit_prof/:id', async (req, res) => {
     await acompanhamentoController.atualizarAcompanhamento(req, res);       //FUNCIONANDO
 });
 
 // Rota para recuperar informações de um acompanhamento
-router.get('/recuperar-acompanhamento/:id', async (req, res) => {           //FUNCIONANDO
+router.get('/recuperar_prof/:id', async (req, res) => {                     //FUNCIONANDO
     await acompanhamentoController.recuperarAcompanhamento(req, res);
 });
 
-// Rota para excluir um acompanhamento (soft delete)
-router.delete('/excluir-acompanhamento/:id', async (req, res) => {          //FUNCIONANDO
+// Rota para excluir um acompanhamento (soft delete)    
+router.delete('/excluir_prof/:id', async (req, res) => {                    //FUNCIONANDO
     await acompanhamentoController.excluirAcompanhamento(req, res);
 });
 
